8ffc741e259567e7cdf3ebc7578972e087b8894d
Courtney Schild <courtneyschild@gmail.com>
2022-04-13 16:51:30 -0700

    add test to check for elements

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,5 +7,11 @@ describe('Website Widget', () => {
   it('should visit the baseUrl', () => {
     cy.url().should('eq', Cypress.config().baseUrl)
   });
+
+  it('should check for h1 and iframes', () => {
+    cy.contains('h1', 'All we have to decide is what to do with the time that is given us.');
+    cy.get('#podium-prompt').should('be.visible');
+    cy.get('#podium-bubble').should('be.visible');
+  });
   
- })
+})
