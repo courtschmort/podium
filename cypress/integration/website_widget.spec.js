0ce19a1b076e8f6d2f1381addbaa94fccbc0d173
Courtney Schild <courtneyschild@gmail.com>
2022-04-13 20:28:39 -0700

    add another test to open and close iframe, refactor tests, and rename spec

--- cypress/integration/website_widget.spec.js
+++ cypress/integration/website_widget.spec.js
@@ -19,14 +19,23 @@ describe('Website Widget', () => {
   });
   
   it('should close the prompt', () => {
+    cy.get('#podium-prompt').should('exist');
     cy.getIframeBody('#podium-prompt').find('button').should('have.text', 'close').click();
     cy.get('#podium-prompt').should('not.exist');
   });
 
-  it('should open and close the bubble', () => {
-    cy.getIframeBody('#podium-bubble').find('button').invoke('attr', 'aria-label').should('include', 'open');
+  it('should open and close the modal by toggling the bubble', () => {
     websiteWidget.toggleBubble();
     cy.getIframeBody('#podium-bubble').find('button').invoke('attr', 'aria-label').should('include', 'close');
+    websiteWidget.toggleBubble();
+    cy.getIframeBody('#podium-bubble').find('button').invoke('attr', 'aria-label').should('include', 'open');
+  });
+
+  it('should open and close the modal by toggling the bubble and clicking outside of the modal', () => {
+    websiteWidget.toggleBubble();
+    cy.get('#podium-modal').should('exist');
+    cy.getIframeBody('#podium-modal').click(0, 0);
+    cy.get('#podium-modal').should('not.exist');
   });
   
 })
